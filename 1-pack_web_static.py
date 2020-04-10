1c12096110cd26f937f96fe0e5ea6f674e250425
danielcinome <danielchinome987@gmail.com>
2020-04-10 15:56:59 +0000

    [Task 1] Add print

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,15 +2,22 @@
 """ Compress before sending """
 from fabric.api import *
 from datetime import datetime
+from os.path import getsize
 
 
 def do_pack():
+    """
+       do_pack
+    """
     now = datetime.now()
     time = now.strftime("%Y%m%d%H%M%S")
     local("mkdir versions")
     path_file = local(
         "tar -cvzf versions/web_static_{}.tgz web_static".format(time))
+    size = getsize("versions/web_static_{}.tgz".format(time))
     if path_file.succeeded:
+        print("web_static packed: versions/web_static_{}.tgz ->\
+ {}Bytes".format(time, size))
         return('versions/web_static_{}.tgz'.format(time))
     else:
         return False
