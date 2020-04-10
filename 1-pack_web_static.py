9c110d26e8ff19e39fda855a7a741934aad6661f
danielcinome <danielchinome987@gmail.com>
2020-04-10 16:13:14 +0000

    [Task] Correction return None

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,9 +8,9 @@ from os.path import getsize
 def do_pack():
     now = datetime.now()
     time = now.strftime("%Y%m%d%H%M%S")
-    local("mkdir versions")
+    local("mkdir -p versions")
     path_file = local(
-        "tar -cvzf versions/web_static_{}.tgz ./web_static".format(time))
+        "tar -cvzf versions/web_static_{}.tgz web_static".format(time))
     size = getsize("versions/web_static_{}.tgz".format(time))
     if path_file.succeeded:
         print("web_static packed: versions/web_static_{}.tgz ->\
