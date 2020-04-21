d91e7600b81025deca1cac7555cc3ff974c4e080
danielcinome <danielchinome987@gmail.com>
2020-04-21 20:49:50 +0000

    [Task 0:6]

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Web Flask Aplication """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,10 @@
+# AirBnB clone - Web framework
+![N|Solid](https://www.holbertonschool.com/holberton-logo.png)
+--
+![N|Solid](https://s3.amazonaws.com/intranet-projects-files/concepts/74/hbnb_step3.png)
+### Install Flask
+
+```
+$ pip3 install Flask
+```
+
