c0f96f75903e361ca10fc91c52451462317c53a4
danielcinome <danielchinome987@gmail.com>
2020-04-22 03:44:46 +0000

    [Task 8] Completed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 """ List of states """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_app_context(self):
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = []
+    states = storage.all(State)
+    return render_template("7-states_list.html", States=states)
+
+
+app.run(host="0.0.0.0", port=5000)
